61bf93a5b8f17f92370baadc61dee9d904a7565b
Lucas Snel <7912315+elbojoloco@users.noreply.github.com>
2021-03-15 09:54:50 +0100

    Test routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test-event', function () {
+    broadcast(new \App\Events\TestEvent);
+});
+
+Route::get('/test-event-others', function () {
+    broadcast(new \App\Events\TestEvent)->toOthers();
+});
